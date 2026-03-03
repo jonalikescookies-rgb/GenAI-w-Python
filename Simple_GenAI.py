e9d38c6a87c7658cd50a84a07cca10211ab08a3d
jonalikescookies-rgb <jonalikescookies@gmail.com>
2026-03-03 18:31:51 +0100

    Update Simple_GenAI.py

--- Simple_GenAI.py
+++ Simple_GenAI.py
@@ -1,5 +1,5 @@
 from google import genai
-client=genai.Client(api_key="AIzaSyDbYT_dMpPWJTkS9Rl89oL_KjsHIVhl2Z4")
+client=genai.Client(api_key="")
 response= client.models.generate_content(
     model="gemini-2.5-flash",
     contents="hello")
